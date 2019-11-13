365fdb0ddc2323c32fb72a29fd76c93a8f179336
Deonte Cooper <deonte.cooper12@gmail.com>
2019-11-13 12:29:56 -0700

    Add event listener to update cards after submission on the form

--- src/Form.js
+++ src/Form.js
@@ -10,6 +10,10 @@ class Form extends Component {
     }
   }
 
+  handleChange = event {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
     return (
       <form>
@@ -18,6 +22,7 @@ class Form extends Component {
           placeholder='Title'
           name='title'
           value={this.state.title}
+          onChange={event => this.handleChange(event)}
         />
 
         <input
@@ -25,6 +30,7 @@ class Form extends Component {
           placeholder='Desciption'
           name='description'
           value={this.state.description}
+          onChange={event => this.handleChange(event)}
         />
 
         <button>SUBMIT</button>
